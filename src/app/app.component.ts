9f7567ef658085137f787b0cb89a03656a7dbddb
Sadhana <sadhanakamma@gmail.com>
2018-02-04 17:42:36 +0530

    List Changed
    Heros replaced with persons.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,16 @@ export class AppComponent {
   title = 'Angular 4';
   selectedHero:any;
   heroes=[
-  {id:1, name: "Hero 1"},
-  {id:2, name: "Hero 2"},
-  {id:3, name: "Hero 3"},
-  {id:4, name: "Hero 4"},
-  {id:5, name: "Hero 5"},
-  {id:6, name: "meghana"},
-  {id:7, name: "sagari"},
-  {id:8, name: "sangavi"},
-  {id:9, name: "swapna"},
-  {id:10, name: "parimala"}
+  {id:1, name: "Person 1"},
+  {id:2, name: "Person 2"},
+  {id:3, name: "Person 3"},
+  {id:4, name: "Person 4"},
+  {id:5, name: "Person 5"},
+  {id:6, name: "Person 6"},
+  {id:7, name: "Person 7"},
+  {id:8, name: "Person 8"},
+  {id:9, name: "Person 9"},
+  {id:10, name: "Person 10"}
   ];
   onSelect(hero) {
     this.selectedHero = hero;
