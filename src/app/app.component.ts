005c4de7b6ff0b80d90a2ee815802198b0faba16
SreekanthKarini <sreekanthkarini@gmail.com>
2018-02-04 17:25:54 +0530

    List Changes
    - Title changed
    - Names Changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'Angular 4';
   selectedHero:any;
   heroes=[
-  {id:1, name: "sadhana"},
-  {id:2, name: "ramesh"},
-  {id:3, name: "sathish"},
-  {id:4, name: "vlakshmi"},
-  {id:5, name: "padma"},
+  {id:1, name: "Hero 1"},
+  {id:2, name: "Hero 2"},
+  {id:3, name: "Hero 3"},
+  {id:4, name: "Hero 4"},
+  {id:5, name: "Hero 5"},
   {id:6, name: "meghana"},
   {id:7, name: "sagari"},
   {id:8, name: "sangavi"},
